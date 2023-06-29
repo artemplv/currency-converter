61c2575faa9105e4f9e4fbe9b5313a101362b986
artemplv <artemplv@gmail.com>
2023-06-29 14:05:00 -0400

    fix husky

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import Example from './scripts/example';
 
 document.addEventListener('DOMContentLoaded', () => {
   const main = document.getElementById('main');
+  // eslint-disable-next-line no-new
   new Example(main);
 });
--- src/scripts/example.js
+++ src/scripts/example.js
@@ -8,7 +8,7 @@ class Example {
   }
 
   handleClick() {
-    this.ele.children[0].innerText = 'Ouch!'
+    this.ele.children[0].innerText = 'Ouch!';
   }
 }
 
